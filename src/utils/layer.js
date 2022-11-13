9371b6a6d26b9045c25176e6af0184d00d4103c4
Sanjaya Thapa <57608136+Anil-account@users.noreply.github.com>
2022-11-13 19:27:02 +0545

    animation

---
+++ src/utils/layer.js
@@ -0,0 +1,11 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import gsap from "gsap";
+
+const loader = new GLTFLoader();
+let flag = true;
+
+export function init() {
+}
+
+// animation
